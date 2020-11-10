58c03e6a43fa77c4651f868a7bfc9aa6f514ce43
TheHunter896 <david_corp896@outlook.es>
2020-11-10 22:00:58 +0100

    Added tests for custom Dataframe

---
+++ classAjson/test_weirdDataframe.py
@@ -0,0 +1,25 @@
+from .transform import weirdDataframe
+import unittest
+import collections
+
+class testWeirdDataframe(unittest.TestCase):
+
+    def test_canDefineAttributesWithIndex(self):
+        df = weirdDataframe()
+
+        df[0] = [0, 1, 2, 3]
+        df[1] = [1, 234, 5, 5]
+
+        self.assertEqual(df['0'], [0, 1, 2, 3])
+        self.assertEqual(df['1'], [1, 234, 5, 5])
+
+    def test_canLoopOverAttributes(self):
+        df = weirdDataframe()
+
+        df[0] = [0,1,2,3]
+        df[1] = [1,234,5,5]
+
+        self.assertEqual([element for element in df], [df['0'], df['1']])
+
+
+
